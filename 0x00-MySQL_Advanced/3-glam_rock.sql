6f68dd1f955af731c0b9e2be3da8d59e8f26af30
Isquare007 <akibrahiim2019@gmail.com>
2023-04-07 00:33:53 +0000

    3- glam rock

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,7 +1,7 @@
 -- ists all bands with Glam rock as their main style, ranked by their longevity
 
 SELECT band_name,
-  COALESCE(split, 2022) - formed as lifespan
+COALESCE(split, 20223) - formed as lifespan
 FROM metal_bands
 WHERE style LIKE '%Glam rock%'
 ORDER BY lifespan DESC;
