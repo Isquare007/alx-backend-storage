03371a6502665eb70ecef51b6d06221f09c1e10c
Isquare007 <akibrahiim2019@gmail.com>
2023-04-08 06:04:41 +0000

    7 abg score

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -5,9 +5,9 @@ DELIMITER $$
 CREATE PROCEDURE ComputeAverageScoreForUser(
 	IN user_id INT)
 BEGIN
-	DECLARE avg_socre FLOAT;
+	DECLARE avg_score FLOAT;
 	SET avg_score = (SELECT AVG(score) FROM CORRECTIONS AS C WHERE C.user_id=user_id);
 	UPDATE users SET average_score = avg_score WHERE id=user_id;
 END
 $$
-DELIMETER ;
+DELIMITER ;
